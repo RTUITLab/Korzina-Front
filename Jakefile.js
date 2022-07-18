65a743ff6ad53e3c0d6321c5e3c8c51ce4f5fc20
Zayac11 <brunogrbstudios@gmail.com>
2022-07-18 23:38:18 +0300

    split courses.json file

--- Jakefile.js
+++ Jakefile.js
@@ -1,14 +1,34 @@
 const fs = require("fs");
-const data = require('./data/courses.json')
 const { readdir } = require('fs').promises;
 const exec = require("child_process").exec;
 
 task("default", [
-    "create course.js file",
+    "create courseData.js file",
     "create footer.pug file",
     "create profile files",
     "build prod version",
 ], function () {
+});
+
+desc("Creating courseData.js file from /data/courses/");
+task("create courseData.js file", function () {
+    return new Promise(async (resolve, reject) => {
+        const dir = await readdir('./data/courses');
+        fs.rmSync('./src/data', { recursive: true, force: true });
+        fs.mkdirSync('./src/data');
+
+        let result = [];
+        let file;
+
+        for (let i of dir) {
+            let data = fs.readFileSync('./data/courses/' + i, 'utf-8')
+            result.push(JSON.parse(data))
+        }
+        result.sort((a,b) => (a.position > b.position) ? 1 : ((b.position > a.position) ? -1 : 0));
+        file = "const data = " + JSON.stringify(result) + "\nexport default data;";
+        fs.writeFileSync(`./src/data/coursesData.js`, file, "utf-8");
+        resolve();
+    });
 });
 
 desc("Creating footer.pug file from /data/footer.json");
@@ -24,30 +44,26 @@ task("create footer.pug file", function () {
     });
 });
 
-desc("Creating course.js file from /data/courses.json");
-task("create course.js file", function () {
-    return new Promise((resolve, reject) => {
-        let data = require('./data/courses.json')
-        let file;
-        fs.rmSync('./src/data', { recursive: true, force: true });
-        fs.mkdirSync('./src/data');
-
-        file = "const data = " + JSON.stringify(data.courses) + "\nexport default data;";
-        fs.writeFileSync(`./src/data/coursesData.js`, file, "utf-8");
-        resolve();
-    });
-});
-
-desc("Creating profile .pug files from /data/courses.json");
+desc("Creating profile .pug files from /data/courses/");
 task("create profile files", function () {
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
+        const dir = await readdir('./data/courses');
         fs.rmSync('./src/profiles', { recursive: true, force: true });
         fs.mkdirSync('./src/profiles');
-        let data = require('./data/courses.json')
 
-        for (let j in data.courses) {
-            fs.writeFileSync(`./src/profiles/${data.courses[j].fileName}`, "extends ../layout/profilePageTemplate/profilePageTemplate.pug\n\nblock variables\n\t-\n\t\tlet obj = " + JSON.stringify(data.courses[j]), 'utf-8');
+        let result = [];
+        for (let i of dir) {
+            let data = fs.readFileSync('./data/courses/' + i, 'utf-8')
+            result.push(JSON.parse(data))
         }
+        result.sort((a,b) => (a.position > b.position) ? 1 : ((b.position > a.position) ? -1 : 0));
+        for (let j in result) {
+            fs.writeFileSync(`./src/profiles/${dir[j]}.pug`,  "extends ../layout/profilePageTemplate/profilePageTemplate.pug\n\nblock variables\n\t-\n\t\tlet obj = " + JSON.stringify(result[j]), 'utf-8');
+        }
+
+        // for (let j in data.courses) {
+        //     fs.writeFileSync(`./src/profiles/${data.courses[j].fileName}`, "extends ../layout/profilePageTemplate/profilePageTemplate.pug\n\nblock variables\n\t-\n\t\tlet obj = " + JSON.stringify(data.courses[j]), 'utf-8');
+        // }
         resolve();
     });
 })
